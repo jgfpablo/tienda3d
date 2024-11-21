80dd89765a63f6a837dc96ea9a876b902e5fbf24
Pablo Frank <jgfpablo@gmail.com>
2024-11-21 02:33:59 -0300

    cambios

--- src/app/store/home-page/home-page.component.ts
+++ src/app/store/home-page/home-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
